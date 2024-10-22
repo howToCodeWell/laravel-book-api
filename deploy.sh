c08b2bc12f0eb725afc1395439c91ce59eae1fa1
pfwd <hello@websomatic.co.uk>
2024-10-22 09:31:04 +0100

    Updating deployment

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,19 @@
 
 echo "Running deploy script \n"
 
-echo "1/4 Pull new changes\n"
+echo "1/5 Pull new changes\n"
 git pull origin main
 
-echo "2/4 Create database if one isn't found \n"
+echo "2/5 Create database if one isn't found \n"
 touch database/database.sqlite
 
-echo "3/4 Installing packages using composer\n"
+echo "3/5 Installing packages using composer\n"
 composer install
 
-echo "4/4 Publishing API Platform assets\n"
+echo "4/5 Publishing API Platform assets\n"
 php artisan api-platform:install
 
-echo "5/4 Migrating database\n"
+echo "5/5 Migrating database\n"
 php artisan migrate
 
 echo "Site has been deployed!\n"
