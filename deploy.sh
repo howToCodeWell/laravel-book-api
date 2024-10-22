edbcd6c2e3a2dadc631a276392c1fde9c89627cc
pfwd <hello@websomatic.co.uk>
2024-10-22 09:30:24 +0100

    Adding deploy

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ echo "Running deploy script \n"
 echo "1/4 Pull new changes\n"
 git pull origin main
 
-echo "3/4 Create database if one isn't found \n"
+echo "2/4 Create database if one isn't found \n"
 touch database/database.sqlite
 
-echo "2/4 Installing packages using composer\n"
+echo "3/4 Installing packages using composer\n"
 composer install
 
 echo "4/4 Publishing API Platform assets\n"
